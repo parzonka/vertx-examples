2d947cc9ac43592a815453ab1d601e3514d639db
Mateusz Parzonka <parzonka@gmail.com>
2016-02-27 18:03:30 +0100

    Factor out handler as consumer

--- src/main/java/http/MainVerticle.java
+++ src/main/java/http/MainVerticle.java
@@ -1,20 +1,26 @@
 package http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
+
+	private static Handler<RoutingContext> html(String html) {
+		return (RoutingContext rc) -> {
+			HttpServerResponse response = rc.response();
+			response.putHeader("content-type", "text/html").end(html);
+		};
+	}
 
 	@Override
 	public void start() {
 
 		Router router = Router.router(vertx);
 
-		router.route("/").handler(routingContext -> {
-			HttpServerResponse response = routingContext.response();
-			response.putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
-		});
+		router.route("/").handler(html("<h1>Hello World!</h1>"));
 
 		vertx.createHttpServer().requestHandler(router::accept)
 				.listen(Integer.getInteger("server.port", 8080), System.getProperty("server.host", "0.0.0.0"));
