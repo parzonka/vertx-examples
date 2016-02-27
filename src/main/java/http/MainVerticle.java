a3649769bfe29ac2e3e255afef170618a1c5ffe9
Mateusz Parzonka <parzonka@gmail.com>
2016-02-27 17:06:51 +0100

    Add Eclipse launch config for continuous redeploy

--- src/main/java/http/MainVerticle.java
+++ src/main/java/http/MainVerticle.java
@@ -2,12 +2,12 @@ package http;
 
 import io.vertx.core.AbstractVerticle;
 
-public class Server extends AbstractVerticle {
+public class MainVerticle extends AbstractVerticle {
 
 	@Override
 	public void start() {
 		vertx.createHttpServer().requestHandler(req -> {
 			req.response().putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
-		}).listen(8081);
+		}).listen(8080);
 	}
 }
