58ce1bb141f32c59ea242960d5105a1aa833633b
Mateusz Parzonka <parzonka@gmail.com>
2016-02-27 17:25:48 +0100

    Fix heroku port property

--- src/main/java/http/MainVerticle.java
+++ src/main/java/http/MainVerticle.java
@@ -8,6 +8,6 @@ public class MainVerticle extends AbstractVerticle {
 	public void start() {
 		vertx.createHttpServer().requestHandler(req -> {
 			req.response().putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
-		}).listen(Integer.getInteger("http.port", 8080), System.getProperty("http.address", "0.0.0.0"));
+		}).listen(Integer.getInteger("server.port", 8080), System.getProperty("server.host", "0.0.0.0"));
 	}
 }
