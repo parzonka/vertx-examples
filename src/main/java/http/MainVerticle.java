c18970a075f86158cf4fbbf4fc2b450b2216ced2
Mateusz Parzonka <parzonka@gmail.com>
2016-02-27 17:21:00 +0100

    Make host and port config heroku compatible

--- src/main/java/http/MainVerticle.java
+++ src/main/java/http/MainVerticle.java
@@ -8,6 +8,6 @@ public class MainVerticle extends AbstractVerticle {
 	public void start() {
 		vertx.createHttpServer().requestHandler(req -> {
 			req.response().putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
-		}).listen(8080);
+		}).listen(Integer.getInteger("http.port", 8080), System.getProperty("http.address", "0.0.0.0"));
 	}
 }
