cfec8d53120d47619dd5a37711194203bdb65bd1
Mateusz Parzonka <parzonka@gmail.com>
2016-02-27 17:55:32 +0100

    Use vertx web

--- src/main/java/http/MainVerticle.java
+++ src/main/java/http/MainVerticle.java
@@ -1,13 +1,22 @@
 package http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
 	@Override
 	public void start() {
-		vertx.createHttpServer().requestHandler(req -> {
-			req.response().putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
-		}).listen(Integer.getInteger("server.port", 8080), System.getProperty("server.host", "0.0.0.0"));
+
+		Router router = Router.router(vertx);
+
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html").end("<h1>Hello World!</h1>");
+		});
+
+		vertx.createHttpServer().requestHandler(router::accept)
+				.listen(Integer.getInteger("server.port", 8080), System.getProperty("server.host", "0.0.0.0"));
 	}
 }
